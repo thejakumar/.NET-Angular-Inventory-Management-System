d4631ce65825224c83e3391863d2f86f127e8089
Theja <Theja@DESKTOP-4BB5QCR>
2023-12-31 00:16:45 -0500

    Add project files.

---
+++ CrudOperationsApp/Migrations/20231127051624_Initial.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CrudOperationsApp.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Brands",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Category = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    IsActive = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Brands", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Brands");
+        }
+    }
+}
---
+++ CrudOperationsApp/Models/Brand.cs
@@ -0,0 +1,10 @@
+﻿namespace CrudOperationsApp.Models
+{
+    public class Brand
+    {
+        public int ID { get; set; }
+        public string? Name { get; set; }
+        public string? Category { get; set; }
+        public int IsActive { get; set; }
+    }
+}
